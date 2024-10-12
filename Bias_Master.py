54b9f9baada185da93a0e99ccccbefa7a8a14c82
s2220961 <152225567+s2220961@users.noreply.github.com>
2024-10-12 05:46:38 +0100

    Update Bias_Master.py
    Simplifies the code a bit and also put some stuff into a function so that it doesnt interrupt and slow down the code and looks better

--- Bias_Master.py
+++ Bias_Master.py
@@ -6,21 +6,15 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from Main import fits_data  # Importing fits_data from Main.py
 
-# Function to process and stack bias frames from a list of file paths
-def process_bias(bias_files):
 
-    # Stack the bias frames (using median stacking to reduce noise)
+def process_bias(bias_files, show_plot=True):
     master_bias = np.median(bias_files, axis=0)
+    if show_plot:
+        plt.imshow(master_bias, cmap='hot', origin='lower')  # Plot the master bias frame
+        plt.colorbar(format='%.2f')
+        plt.title("Master Bias Frame")
+        plt.show()
     return master_bias
 
 bias_files = fits_data['Calibration']['Bias']
-
-master_bias = process_bias(bias_files)
-
-# Plot the master bias frame
-plt.imshow(master_bias, cmap='hot', origin='lower')
-plt.colorbar(format='%.2f')  # Format the color bar with floating point values
-plt.title("Master Bias Frame")
-plt.show()
-
-print("Script execution complete.")
+master_bias = process_bias(bias_files, show_plot=False)
