f05b30a7b79f9a84f5f5b942766ee9f7b6e300e3
s2220961 <152225567+s2220961@users.noreply.github.com>
2024-10-15 22:10:53 +0100

    Update Bias_Master.py

--- Bias_Master.py
+++ Bias_Master.py
@@ -5,11 +5,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from Main import fits_data  # Importing fits_data from Main.py
-
+from matplotlib import colors
 
 def process_bias(bias_files, show_plot=True):
     master_bias = np.median(bias_files, axis=0)
     if show_plot:
+        plt.imshow(master_bias, cmap='hot', origin='lower' , norm=colors.LogNorm(vmin=np.percentile(master_bias, 5), vmax=np.percentile(master_bias, 95))) # Plot the master bias frame
         plt.imshow(master_bias, cmap='hot', origin='lower')  # Plot the master bias frame
         plt.colorbar(format='%.2f')
         plt.title("Master Bias Frame")
