9eb1f600d252abf292c6861ea7e59143b6ac8e5f
s2220961 <152225567+s2220961@users.noreply.github.com>
2024-11-03 11:18:34 +0000

    Update Bias_Master.py

--- Bias_Master.py
+++ Bias_Master.py
@@ -1,19 +1,30 @@
-''' WORKS AS INTENDED '''
-''' The script processes all of our Bias frame files, creates and then plots the Master '''
-
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
 from Main import Bias  # Importing Bias from Main.py
 from matplotlib import colors
+import os
 
-def process_bias(bias_files, show_plot=True):
-    master_bias = np.median(bias_files, axis=0)
+def process_bias(bias_files, show_plot=True, save_path=None):
+    master_bias = np.mean(bias_files, axis=0)
+
     if show_plot:
-        plt.imshow(master_bias, cmap='hot', origin='lower' , norm=colors.LogNorm(vmin=np.percentile(master_bias, 5), vmax=np.percentile(master_bias, 95))) # Plot the master bias frame
+        plt.imshow(master_bias, cmap='hot', origin='lower', norm=colors.LogNorm(vmin=np.percentile(master_bias, 5), vmax=np.percentile(master_bias, 95)))
         plt.colorbar(format='%.2f')
         plt.title("Master Bias Frame")
         plt.show()
+
+    # Save the master bias
+    if save_path:
+        os.makedirs(os.path.dirname(save_path), exist_ok=True) # Ensure the folder exists
+        hdu = fits.PrimaryHDU(master_bias)
+        hdu.writeto(save_path, overwrite=True)
+        print(f"Master bias frame saved to {save_path}")
+    
     return master_bias
 
-master_bias = process_bias(Bias, show_plot=False)  # Process the Bias frames and plot the master bias frame
+save_folder = 'G:/MyProject/TGP/data_reduction/Master_Bias'
+save_filename = 'master_bias.fits'
+save_path = os.path.join(save_folder, save_filename)
+
+master_bias = process_bias(Bias, show_plot=True, save_path=save_path)
